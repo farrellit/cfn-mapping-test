e7815503ceb7487ad722f7ea0a3afe5ed7b03b14
farrellit <farrellit@github>
2024-01-17 17:02:35 -0600

    reproduction of issue for SO
    https://stackoverflow.com/questions/77836011/aws-cdk-cfnmapping-value-as-list-type-in-aws-cdk-aws-apigatewayv2-authorizers

--- cdk_mapping_test/cdk_mapping_test_stack.py
+++ cdk_mapping_test/cdk_mapping_test_stack.py
@@ -1,7 +1,9 @@
 from aws_cdk import (
-    # Duration,
+    CfnMapping,
+    CfnOutput,
+    Fn,
     Stack,
-    # aws_sqs as sqs,
+    aws_apigatewayv2_authorizers
 )
 from constructs import Construct
 
@@ -10,10 +12,23 @@ class CdkMappingTestStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        mapping = CfnMapping(self, "Mapping",
+            mapping={
+                "us-east-2": { "values": [ "value1", "value2" ] }
+            },
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkMappingTestQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        authorizer = aws_apigatewayv2_authorizers.HttpJwtAuthorizer(
+            "HttpJWTAuthorizer",
+            "auther",
+            authorizer_name="JWT-Authorizer",
+            identity_source=["$request.header.Authorization"],
+            #jwt_audience=["value1", "value2"], # synth works
+            jwt_audience=mapping.find_in_map(Fn.ref("AWS::Region"), "values"), # synth does not work
+        )
+        # fails: "Template format error: The Value field of every Outputs member must evaluate to a String and not a List."
+        #CfnOutput(
+        #    self,
+        #    "values",
+        #    value=mapping.find_in_map(Fn.ref("AWS::Region"), "values")
+        #)
